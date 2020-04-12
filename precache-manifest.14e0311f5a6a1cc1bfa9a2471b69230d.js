ddb008bde750bc3ce2d80b32b53b456a8cbec5a1
MaxAlter <alter0075926@gmail.com>
2020-04-12 11:12:12 +0300

    Updates

---
+++ precache-manifest.14e0311f5a6a1cc1bfa9a2471b69230d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b208f6b9b2c69349276ebb07828424",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "5911704d4990cb82bac7",
+    "url": "/goit-react-hw-01-components/static/css/main.6d911e39.chunk.css"
+  },
+  {
+    "revision": "248e3384351284ff0b52",
+    "url": "/goit-react-hw-01-components/static/js/2.602fc3c1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.602fc3c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5911704d4990cb82bac7",
+    "url": "/goit-react-hw-01-components/static/js/main.1afe5f07.chunk.js"
+  },
+  {
+    "revision": "3b854d41955e63455d3a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.68b1ab02.js"
+  }
+]);
